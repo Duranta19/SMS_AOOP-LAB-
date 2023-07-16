e564047192e34c39fd063204db6296176d4c7e2c
Sayem Siam <sayemsiam198@gmail.com>
2023-07-16 20:51:00 +0600

    upload

---
+++ src/main/java/com/example/assignment1/UpdateCon.java
@@ -0,0 +1,18 @@
+package com.example.assignment1;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class UpdateCon {
+
+    @FXML
+    void backgroundUpdate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void marksUpdate(ActionEvent event) {
+
+    }
+
+}
